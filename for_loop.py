f79ba83acf6cee4a5aaec5878c0f19c455c83026
ShaikMustaqSharif <shaikmustaqsharif@gmail.com>
2026-06-29 23:28:15 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+tuple1=(2,56,34,3,5,-1)
+for i in tuple1:
+    print(i)
+else:
+    print("Loop successfully completed and we are in else blook now!!!")
+
+tuple2=(2,56,34,3,5,-1)
+for i in tuple2:
+    print(i)
+    if i%6 ==0:
+        break
+    else:
+      print("there no number that divisible by 6")
+
---
+++ loops.py
@@ -0,0 +1,24 @@
+name='Jenny'
+for i in name:
+   print(i)
+
+
+names=['jenny','Ram','Shyam']
+for i in names:
+    print(i)
+
+#nums=[2,3,5,-2,10]
+#for i in nums:
+    #square=i**2
+   # print(square)
+
+nums_1=[2,3,5,-2,10]
+squares=[]
+for i in nums_1:
+    square=i**2
+    squares.append(square)
+    print(squares)
+
+
+
+
