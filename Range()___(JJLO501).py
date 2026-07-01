d4ee33192afce59af23d4afdff5bd64474b9498a
ShaikMustaqSharif <shaikmustaqsharif@gmail.com>
2026-07-01 22:17:19 +0530

    Add files via upload

---
+++ Range()___(JJLO501).py
@@ -0,0 +1,23 @@
+for i in range(5):
+    print(i)
+
+print()
+
+for i in range(2,6):
+    print(i)
+print()
+
+a=range(2,15,2)
+for i in  a:
+    print(i)
+
+print()
+
+b=range(-1,-10,-1)
+for i in b:
+    print(i)
+
+total=0
+for i in range(1,101):
+    total+=i
+print(total)
---
+++ while_loop__(JJL05401).py
@@ -0,0 +1,18 @@
+count=1
+while count<=5:
+    print(count)
+    count+=1
+print("out from loop")
+print()
+count=5
+while count>0:
+    print(count)
+    count-=1
+print("out from loop")
+print()
+count=5
+while count>0: print(count); count-=1
+print("out from loop")
+
+
+
