23e888d443e153bb41d4df0721e8c9cef651af8b
ShaikMustaqSharif <shaikmustaqsharif@gmail.com>
2026-07-02 13:04:06 +0530

    Add files via upload

--- while_loop__(JJL05401).py
+++ while_loop__(JJL05401).py
@@ -14,5 +14,25 @@ count=5
 while count>0: print(count); count-=1
 print("out from loop")
 
+print()
+print()
+
+count=5
+while count>0:
+    print(count); count-=1;
+    if count ==3:
+        break
+    else:
+        print("in else block")
+print("out from loop")
+
+number =int(input("Enter a number(-1 to quit):"))
+while number  !=-1:
+    print(number)
+    number = int(input("Enter a number(-1 to quit):"))
+else :
+    print("in else block")
+print("out from loop")
+
 
 
---
+++ while_sum+ve number___(JJL05401).PY.py
@@ -0,0 +1,7 @@
+total=0
+numbers=int(input("Enter a numbers:"))
+while numbers!=0:
+    total+=numbers
+    numbers=int(input("Enter a numbers:"))
+print(total)
+
