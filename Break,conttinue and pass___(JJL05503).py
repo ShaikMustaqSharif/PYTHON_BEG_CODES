12fdba9e579a6368563aa9f041c793198c87b3c2
ShaikMustaqSharif <shaikmustaqsharif@gmail.com>
2026-07-03 23:11:31 +0530

    Add files via upload

---
+++ Break,conttinue and pass___(JJL05503).py
@@ -0,0 +1,19 @@
+count=1
+while count<=10:
+    print(count)
+    count+=1
+    if count==7:
+        break
+    print("hi")
+print("out of the loop")
+
+count=1
+while count<=10:
+    print(count)
+    count+=1
+    if count==7:
+     continue
+    print("Hi")
+print("out of the loop")
+
+
---
+++ identation___(JJL05603).py
@@ -0,0 +1,9 @@
+for i in range(1,11):
+    print(i)
+    print("hi")
+    if i==3:
+        print("welcome")
+        if True:
+            print("hi")
+
+print("bye take care")
